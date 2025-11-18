3d69135d3d19560d3dec6d90f404bbdbb320d3b8
jaguarrr <59565481+jaguarsoff@users.noreply.github.com>
2025-11-19 02:31:58 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str = "YOUR_TOKEN"
-    ADMIN_ID: int = 123
+    BOT_TOKEN: str = "8288012104:AAEy_eFaSjwG8DGBQaRPQGy2cG3pFpCzTqE"
+    ADMIN_ID: int = 8254985499
 
 settings = Settings()
