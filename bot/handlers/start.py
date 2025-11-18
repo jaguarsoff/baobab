d69a9160ca6a20f9fd07b8d4d06397a78c9bf200
jaguarrr <59565481+jaguarsoff@users.noreply.github.com>
2025-11-19 01:42:25 +0700

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,9 +1,10 @@
 from aiogram import Router
 from aiogram.types import Message
 from bot.keyboards import main_menu
+from aiogram.filters import Command
 
 router = Router()
 
-@router.message(commands=['start'])
+@router.message(Command("start"))
 async def cmd_start(message: Message):
     await message.answer('Привет! Я Poizon-бот. Выберите действие:', reply_markup=main_menu())
